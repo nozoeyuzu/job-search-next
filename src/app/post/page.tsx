dcfb45722663009f3871c096b3a8c3ecab63fbf9
nozoeyuzu <citron12yz21n82@docomo.ne.jp>
2025-02-01 14:34:13 +0900

    change ui

--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -5,11 +5,11 @@ export default function CreateJobPage(){
   return (
     <div className="min-h-screen flex flex-col">
       {/* ヘッダー */}
-      <header className="bg-blue-900 text-white p-4 flex justify-between">
-        <div className="font-bold text-xl">求人検索アプリ</div>
+      <header className="bg-blue-900 text-white p-6 flex justify-between">
+        <div className="font-bold text-2xl">求人検索アプリ</div>
         <div>
-          <Link href="/" className="mr-4">求人検索</Link>
-          <Link href="/post">求人投稿</Link>
+          <Link href="/" className="mr-4 text-lg">求人検索</Link>
+          <Link href="/post" className="text-lg">求人投稿</Link>
         </div>
       </header>
 
